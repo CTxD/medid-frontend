b8c0464f5b8e5d598772f11e94dde5e69be3d39e
Peter <pmadse13@student.aau.dk>
2019-03-19 09:49:09 +0100

    renamed to follow our conventions

---
+++ lib/src/ui/result_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class ResultPage extends StatelessWidget {
+  final Widget text = Text("Result");
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: text,
+    );
+  }
+}
