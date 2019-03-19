5d5814d5bdc99699e07723aa2d884a8c1e3ea9b5
TeiturMcSwagger <teiturargeseloy@hotmail.com>
2019-03-19 08:20:50 +0100

    Homepage with four buttons that can go to a new widget(take an image, pill library, info and help).

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,148 @@ import 'package:flutter/material.dart';
 import 'src/app.dart';
 
 // Run the main App Widget
-void main() => runApp(App());
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MedID",
+      home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HomePage"),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new RaisedButton(
+              padding: const EdgeInsets.all(8.0),
+              textColor: Colors.white,
+              color: Colors.blue,
+              child:  new Text("Take a picture"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TakeAPicture(),
+                  ),
+                );
+              },
+            ),
+            new RaisedButton(
+              padding: const EdgeInsets.all(8.0),
+              textColor: Colors.white,
+              color: Colors.blue,
+              child:  new Text("Pill library"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PillLibrary(),
+                  ),
+                );
+              },
+            ),
+            new RaisedButton(
+              padding: const EdgeInsets.all(8.0),
+              textColor: Colors.white,
+              color: Colors.blue,
+              child:  new Text("Info"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Info(),
+                  ),
+                );
+              },
+            ),
+            new RaisedButton(
+              padding: const EdgeInsets.all(8.0),
+              textColor: Colors.white,
+              color: Colors.blue,
+              child:  new Text("Help"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Help(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TakeAPicture extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Take a picture"),
+      ),
+      body: new Center(
+        
+      ),
+    );
+  }
+}
+
+class PillLibrary extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pill library"),
+      ),
+      body: new Center(
+        
+      ),
+    );
+  }
+}
+
+class Info extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Info"),
+      ),
+      body: new Center(
+        
+      ),
+    );
+  }
+}
+
+class Help extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Help"),
+      ),
+      body: new Center(
+        
+      ),
+    );
+  }
+}
