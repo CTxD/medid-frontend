b235c3a7ee1c41ac41a14be26477f69659e26e58
Christian Larsen <c@chrl.dk>
2019-03-20 15:09:50 +0100

    enterprise UI

--- lib/src/ui/home_page.dart
+++ lib/src/ui/home_page.dart
@@ -12,112 +12,205 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("HomePage"),
-        ),
-        body: new Column(mainAxisAlignment: MainAxisAlignment.end, children: [
-          new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Expanded(
-                child: new MaterialButton(
-                  key: navigateToTakeAPictureButtonKey,
-                  padding: const EdgeInsets.all(8.0),
-                  textColor: Colors.white,
-                  color: Colors.blue,
-                  height: 150,
-                  child: Column(
+        body: new Container(
+            decoration: new BoxDecoration(color: Colors.white),
+            child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  new Row(
                     children: <Widget>[
-                      Icon(Icons.camera_alt),
-                      Text("Take a picture"),
+                      new Container(
+                        child: new Expanded(
+                          child: new Image.asset('images/MEDID_LOGO.png'),
+                        ),
+                      ),
                     ],
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => TakeAPicture(),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(width: 4),
-              new Expanded(
-                  child: new MaterialButton(
-                    key: navigateToPillLibraryButtonKey,
-                padding: const EdgeInsets.all(8.0),
-                textColor: Colors.white,
-                color: Colors.blue[300],
-                height: 150,
-                child: Column(
-                  children: <Widget>[
-                    Icon(Icons.list),
-                    Text("Pill library"),
-                  ],
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => PillLibrary(),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  new Container(
+                    decoration: new BoxDecoration(
+                        gradient: new RadialGradient(
+                      center: Alignment(0.0, 0.0),
+                      colors: [Colors.lightBlue[300], Colors.white],
+                      radius: 0.5,
+                    )),
+                    child: new Wrap(
+                      children: <Widget>[
+                        new Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new Expanded(
+                                child: new Container(
+                              height: 150,
+                              child: new FlatButton(
+                                child: new Image.asset(
+                                  'images/photo.png',
+                                  height: 70,
+                                  width: 70,
+                                ),
+                                color: Colors.white,
+                                onPressed: () => {},
+                              ),
+                            )),
+                            SizedBox(
+                              width: 2,
+                            ),
+                            new Expanded(
+                                child: new Container(
+                              height: 150,
+                              child: new FlatButton(
+                                  child: new Image.asset(
+                                    'images/list.png',
+                                    height: 55,
+                                    width: 55,
+                                  ),
+                                  color: Colors.white,
+                                  onPressed: () => {}),
+                            ))
+                          ],
+                        ),
+                        SizedBox(
+                          height: 152,
+                        ),
+                        new Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new Expanded(
+                                child: new Container(
+                              height: 100,
+                              child: new FlatButton(
+                                child: new Image.asset(
+                                  'images/info.png',
+                                  height: 35,
+                                  width: 35,
+                                ),
+                                color: Colors.white,
+                                onPressed: () => {},
+                              ),
+                            )),
+                            SizedBox(
+                              width: 2,
+                            ),
+                            new Expanded(
+                                child: new Container(
+                              height: 100,
+                              child: new FlatButton(
+                                  child: new Image.asset(
+                                    'images/help.png',
+                                    height: 35,
+                                    width: 35,
+                                  ),
+                                  color: Colors.white,
+                                  onPressed: () => {}),
+                            ))
+                          ],
+                        ),
+                      ],
                     ),
-                  );
-                },
-              )),
-            ],
-          ),
-          SizedBox(height: 4),
-          new Row(
-            children: <Widget>[
-              new Expanded(
-                child: new MaterialButton(
-                  key: navigateToInfoButtonKey,
-                  padding: const EdgeInsets.all(8.0),
-                  textColor: Colors.white,
-                  color: Colors.blue[300],
-                  child: Column(
-                    children: <Widget>[
-                      Icon(Icons.info),
-                      Text("Info"),
-                    ],
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Info(),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(width: 4),
-              new Expanded(
-                child: new MaterialButton(
-                  key: navigateToHelpButtonKey,
-                  padding: const EdgeInsets.all(8.0),
-                  textColor: Colors.white,
-                  color: Colors.blue[300],
-                  child: Column(
-                    children: <Widget>[
-                      Icon(Icons.help),
-                      Text("Help"),
-                    ],
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Help(),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 4),
-        ]));
+                  // new Row(
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   children: <Widget>[
+                  //     new Expanded(
+                  //       child: new FlatButton(
+                  //         key: navigateToTakeAPictureButtonKey,
+                  //         padding: const EdgeInsets.all(8.0),
+                  //         textColor: Colors.black,
+                  //         color: Colors.white,
+                  //         child: Column(
+                  //           children: <Widget>[
+                  //             Icon(Icons.camera_alt, color: Colors.black),
+                  //             Text("Take a picture"),
+                  //           ],
+                  //         ),
+                  //         onPressed: () {
+                  //           Navigator.push(
+                  //             context,
+                  //             MaterialPageRoute(
+                  //               builder: (context) => TakeAPicture(),
+                  //             ),
+                  //           );
+                  //         },
+                  //       ),
+                  //     ),
+                  //     SizedBox(width: 4),
+                  //     new Expanded(
+                  //         child: new MaterialButton(
+                  //       key: navigateToPillLibraryButtonKey,
+                  //       padding: const EdgeInsets.all(8.0),
+                  //       textColor: Colors.white,
+                  //       color: Colors.blue[300],
+                  //       height: 150,
+                  //       child: Column(
+                  //         children: <Widget>[
+                  //           Icon(Icons.list),
+                  //           Text("Pill library"),
+                  //         ],
+                  //       ),
+                  //       onPressed: () {
+                  //         Navigator.push(
+                  //           context,
+                  //           MaterialPageRoute(
+                  //             builder: (context) => PillLibrary(),
+                  //           ),
+                  //         );
+                  //       },
+                  //     )),
+                  //   ],
+                  // ),
+                  // SizedBox(height: 4),
+                  // new Row(
+                  //   children: <Widget>[
+                  //     new Expanded(
+                  //       child: new MaterialButton(
+                  //         key: navigateToInfoButtonKey,
+                  //         padding: const EdgeInsets.all(8.0),
+                  //         textColor: Colors.white,
+                  //         color: Colors.blue[300],
+                  //         child: Column(
+                  //           children: <Widget>[
+                  //             Icon(Icons.info),
+                  //             Text("Info"),
+                  //           ],
+                  //         ),
+                  //         onPressed: () {
+                  //           Navigator.push(
+                  //             context,
+                  //             MaterialPageRoute(
+                  //               builder: (context) => Info(),
+                  //             ),
+                  //           );
+                  //         },
+                  //       ),
+                  //     ),
+                  //     SizedBox(width: 4),
+                  //     new Expanded(
+                  //       child: new MaterialButton(
+                  //         key: navigateToHelpButtonKey,
+                  //         padding: const EdgeInsets.all(8.0),
+                  //         textColor: Colors.white,
+                  //         color: Colors.blue[300],
+                  //         child: Column(
+                  //           children: <Widget>[
+                  //             Icon(Icons.help),
+                  //             Text("Help"),
+                  //           ],
+                  //         ),
+                  //         onPressed: () {
+                  //           Navigator.push(
+                  //             context,
+                  //             MaterialPageRoute(
+                  //               builder: (context) => Help(),
+                  //             ),
+                  //           );
+                  //         },
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
+                  SizedBox(height: 1),
+                ])));
   }
 }
